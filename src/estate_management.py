01e38b2da73622d5ebd3a0eeba04eeaf777db9d6
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2024-10-16 18:33:03 +0200

    actual monopoly fields

--- src/estate_management.py
+++ src/estate_management.py
@@ -70,7 +70,7 @@ class Estate:
         if not self.mortgaged:
             self.mortgaged = True
             return self.cost // 2
-        return -1
+        return None
 
     def unmortgage_estate(self):
         """
@@ -84,7 +84,7 @@ class Estate:
         if self.mortgaged:
             self.mortgaged = False
             return self.cost // 2
-        return -1
+        return None
 
 
 def initialize_estates():
@@ -95,28 +95,65 @@ def initialize_estates():
     -------
     list
         A list containing all the estates.
+
+    Groups
+    ------
+        0: Go
+        1: Brown
+        2: Light Blue
+        3: Pink
+        4: Orange
+        5: Red
+        6: Yellow
+        7: Green
+        8: Blue
+        9: Railroads
+        10: Utilities
+        11: Chance
+        12: Community Chest
+        13: Tax
+        14: Jail
+        15: Free Parking
+        16: Go to Jail
     """
     estates = []
-    estates.append(Estate("Mediterranean Avenue", 60, 2, 1))
-    estates.append(Estate("Baltic Avenue", 60, 4, 1))
-    estates.append(Estate("Oriental Avenue", 100, 6, 2))
-    estates.append(Estate("Vermont Avenue", 100, 6, 2))
-    estates.append(Estate("Connecticut Avenue", 120, 8, 2))
-    estates.append(Estate("St. Charles Place", 140, 10, 3))
-    estates.append(Estate("States Avenue", 140, 10, 3))
-    estates.append(Estate("Virginia Avenue", 160, 12, 3))
-    estates.append(Estate("St. James Place", 180, 14, 4))
-    estates.append(Estate("Tennessee Avenue", 180, 14, 4))
-    estates.append(Estate("New York Avenue", 200, 16, 4))
-    estates.append(Estate("Kentucky Avenue", 220, 18, 5))
-    estates.append(Estate("Indiana Avenue", 220, 18, 5))
-    estates.append(Estate("Illinois Avenue", 240, 20, 5))
-    estates.append(Estate("Atlantic Avenue", 260, 22, 6))
-    estates.append(Estate("Ventnor Avenue", 260, 22, 6))
-    estates.append(Estate("Marvin Gardens", 280, 24, 6))
-    estates.append(Estate("Pacific Avenue", 300, 26, 7))
-    estates.append(Estate("North Carolina Avenue", 300, 26, 7))
-    estates.append(Estate("Pennsylvania Avenue", 320, 28, 7))
-    estates.append(Estate("Park Place", 350, 35, 8))
-    estates.append(Estate("Boardwalk", 400, 50, 8))
+    estates.append(Estate("Go", 0, 0, 0))
+    estates.append(Estate("Old Kent Road", 60, 2, 1))
+    estates.append(Estate("Community Chest", 0, 0, 12))
+    estates.append(Estate("Whitechapel Road", 60, 4, 1))
+    estates.append(Estate("Income Tax", 200, 0, 13))
+    estates.append(Estate("Kings Cross Station", 200, 25, 9))
+    estates.append(Estate("The Angel Islington", 100, 6, 2))
+    estates.append(Estate("Chance", 0, 0, 11))
+    estates.append(Estate("Euston Road", 100, 6, 2))
+    estates.append(Estate("Pentonville Road", 120, 8, 2))
+    estates.append(Estate("Jail", 0, 0, 14))
+    estates.append(Estate("Pall Mall", 140, 10, 3))
+    estates.append(Estate("Electric Company", 150, 0, 10))
+    estates.append(Estate("Whitehall", 140, 10, 3))
+    estates.append(Estate("Northumberland Avenue", 160, 12, 3))
+    estates.append(Estate("Marylebone Station", 200, 25, 9))
+    estates.append(Estate("Bow Street", 180, 14, 4))
+    estates.append(Estate("Marlborough Street", 180, 14, 4))
+    estates.append(Estate("Vine Street", 200, 16, 4))
+    estates.append(Estate("Free Parking", 0, 0, 15))
+    estates.append(Estate("Strand", 220, 18, 5))
+    estates.append(Estate("Chance", 0, 0, 11))
+    estates.append(Estate("Fleet Street", 220, 18, 5))
+    estates.append(Estate("Trafalgar Square", 240, 20, 5))
+    estates.append(Estate("Fenchurch St. Station", 200, 25, 9))
+    estates.append(Estate("Leicester Square", 260, 22, 6))
+    estates.append(Estate("Coventry Street", 260, 22, 6))
+    estates.append(Estate("Water Works", 150, 0, 10))
+    estates.append(Estate("Piccadilly", 280, 24, 6))
+    estates.append(Estate("Go to Jail", 0, 0, 16))
+    estates.append(Estate("Regent Street", 300, 26, 7))
+    estates.append(Estate("Oxford Street", 300, 26, 7))
+    estates.append(Estate("Community Chest", 0, 0, 12))
+    estates.append(Estate("Bond Street", 320, 28, 7))
+    estates.append(Estate("Liverpool St. Station", 200, 25, 9))
+    estates.append(Estate("Chance", 0, 0, 11))
+    estates.append(Estate("Park Lane", 350, 35, 8))
+    estates.append(Estate("Super Tax", 100, 0, 13))
+    estates.append(Estate("Mayfair", 400, 50, 8))
     return estates
