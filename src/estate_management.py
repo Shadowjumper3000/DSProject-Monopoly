3cec634b6a749f3a39a98b0f33260c6ce79e6f0e
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2024-10-16 15:26:16 +0200

    estate_management - file restruc

--- src/estate_management.py
+++ src/estate_management.py
@@ -1,44 +1,44 @@
 """
-property_management.py
+estate_management.py
 
-This module contains the Property class.
+This module contains the estate class.
 """
 
 
 class Estate:
     """
-    A class to represent a property in the Monopoly game.
+    A class to represent an estate in the Monopoly game.
 
     Attributes
     ----------
     name : str
-        The name of the property.
+        The name of the estate.
     cost : int
-        The cost to purchase the property.
+        The cost to purchase the estate.
     rent : int
-        The rent charged when another player lands on the property.
+        The rent charged when another player lands on the estate.
     street_group : int
-        The group to which the property belongs.
+        The group to which the estate belongs.
     owner : str or None
-        The owner of the property.
+        The owner of the estate.
     mortgaged : bool
-        Indicates if the property is mortgaged.
+        Indicates if the estate is mortgaged.
     """
 
     def __init__(self, name, cost, rent, street_group):
         """
-        Constructs all the necessary attributes for the Property object.
+        Constructs all the necessary attributes for the estate object.
 
         Parameters
         ----------
         name : str
-            The name of the property.
+            The name of the estate.
         cost : int
-            The cost to purchase the property.
+            The cost to purchase the estate.
         rent : int
-            The rent charged when another player lands on the property.
+            The rent charged when another player lands on the estate.
         street_group : int
-            The group to which the property belongs.
+            The group to which the estate belongs.
         """
         self.name = name
         self.cost = cost
@@ -49,37 +49,37 @@ class Estate:
 
     def change_owner(self, new_owner):
         """
-        Changes the owner of the property.
+        Changes the owner of the estate.
 
         Parameters
         ----------
         new_owner : str
-            The new owner of the property.
+            The new owner of the estate.
         """
         self.owner = new_owner
 
-    def mortgage_property(self):
+    def mortgage_esta(self):
         """
-        Mortgages the property if it is not already mortgaged.
+        Mortgages the estate if it is not already mortgaged.
 
         Returns
         -------
         int
-            Half the cost of the property if it is not mortgaged, otherwise -1.
+            Half the cost of the estate if it is not mortgaged, otherwise -1.
         """
         if not self.mortgaged:
             self.mortgaged = True
             return self.cost // 2
         return -1
 
-    def unmortgage_property(self):
+    def unmortgage_estate(self):
         """
-        Unmortgages the property if it is mortgaged.
+        Unmortgages the estate if it is mortgaged.
 
         Returns
         -------
         int
-            The cost to unmortgage the property if it is mortgaged, otherwise -1.
+            The cost to unmortgage the estate if it is mortgaged, otherwise -1.
         """
         if self.mortgaged:
             self.mortgaged = False
@@ -87,36 +87,36 @@ class Estate:
         return -1
 
 
-def initialize_properties():
+def initialize_estates():
     """
-    Initializes the properties for the Monopoly game.
+    Initializes the estates for the Monopoly game.
 
     Returns
     -------
     list
-        A list containing all the properties.
+        A list containing all the estates.
     """
-    properties = []
-    properties.append(Estate("Mediterranean Avenue", 60, 2, 1))
-    properties.append(Estate("Baltic Avenue", 60, 4, 1))
-    properties.append(Estate("Oriental Avenue", 100, 6, 2))
-    properties.append(Estate("Vermont Avenue", 100, 6, 2))
-    properties.append(Estate("Connecticut Avenue", 120, 8, 2))
-    properties.append(Estate("St. Charles Place", 140, 10, 3))
-    properties.append(Estate("States Avenue", 140, 10, 3))
-    properties.append(Estate("Virginia Avenue", 160, 12, 3))
-    properties.append(Estate("St. James Place", 180, 14, 4))
-    properties.append(Estate("Tennessee Avenue", 180, 14, 4))
-    properties.append(Estate("New York Avenue", 200, 16, 4))
-    properties.append(Estate("Kentucky Avenue", 220, 18, 5))
-    properties.append(Estate("Indiana Avenue", 220, 18, 5))
-    properties.append(Estate("Illinois Avenue", 240, 20, 5))
-    properties.append(Estate("Atlantic Avenue", 260, 22, 6))
-    properties.append(Estate("Ventnor Avenue", 260, 22, 6))
-    properties.append(Estate("Marvin Gardens", 280, 24, 6))
-    properties.append(Estate("Pacific Avenue", 300, 26, 7))
-    properties.append(Estate("North Carolina Avenue", 300, 26, 7))
-    properties.append(Estate("Pennsylvania Avenue", 320, 28, 7))
-    properties.append(Estate("Park Place", 350, 35, 8))
-    properties.append(Estate("Boardwalk", 400, 50, 8))
-    return properties
+    estates = []
+    estates.append(Estate("Mediterranean Avenue", 60, 2, 1))
+    estates.append(Estate("Baltic Avenue", 60, 4, 1))
+    estates.append(Estate("Oriental Avenue", 100, 6, 2))
+    estates.append(Estate("Vermont Avenue", 100, 6, 2))
+    estates.append(Estate("Connecticut Avenue", 120, 8, 2))
+    estates.append(Estate("St. Charles Place", 140, 10, 3))
+    estates.append(Estate("States Avenue", 140, 10, 3))
+    estates.append(Estate("Virginia Avenue", 160, 12, 3))
+    estates.append(Estate("St. James Place", 180, 14, 4))
+    estates.append(Estate("Tennessee Avenue", 180, 14, 4))
+    estates.append(Estate("New York Avenue", 200, 16, 4))
+    estates.append(Estate("Kentucky Avenue", 220, 18, 5))
+    estates.append(Estate("Indiana Avenue", 220, 18, 5))
+    estates.append(Estate("Illinois Avenue", 240, 20, 5))
+    estates.append(Estate("Atlantic Avenue", 260, 22, 6))
+    estates.append(Estate("Ventnor Avenue", 260, 22, 6))
+    estates.append(Estate("Marvin Gardens", 280, 24, 6))
+    estates.append(Estate("Pacific Avenue", 300, 26, 7))
+    estates.append(Estate("North Carolina Avenue", 300, 26, 7))
+    estates.append(Estate("Pennsylvania Avenue", 320, 28, 7))
+    estates.append(Estate("Park Place", 350, 35, 8))
+    estates.append(Estate("Boardwalk", 400, 50, 8))
+    return estates
